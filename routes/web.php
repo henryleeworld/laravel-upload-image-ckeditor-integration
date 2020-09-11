9158f0a8446426acc3e864df2d0e40207ae1f6c9
Henry Lee <henryleeworld@gmail.com>
2020-09-11 20:12:24 +0800

    feat:升級至 laravel 8

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CkeditorController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('ckeditor', 'CkeditorController@index');
-Route::post('ckeditor/upload', 'CkeditorController@upload')->name('ckeditor.upload');
+Route::get('ckeditor', [CkeditorController::class, 'index']);
+Route::post('ckeditor/upload', [CkeditorController::class, 'upload'])->name('ckeditor.upload');
