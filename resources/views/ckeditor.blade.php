4fc5b87b6cc2f7a5423d617b241fc34a7944eedf
Henry Lee <henryleeworld@gmail.com>
2022-06-18 22:04:58 +0800

    feat:更新最新資料

--- resources/views/ckeditor.blade.php
+++ resources/views/ckeditor.blade.php
@@ -10,8 +10,8 @@
     </head>
     <body>
         <div id="editor"></div>
-        <script src="https://cdn.ckeditor.com/ckeditor5/33.0.0/classic/ckeditor.js"></script>
-        <script src="https://cdn.ckeditor.com/ckeditor5/33.0.0/classic/translations/zh.js"></script>
+        <script src="https://cdn.ckeditor.com/ckeditor5/34.0.0/classic/ckeditor.js"></script>
+        <script src="https://cdn.ckeditor.com/ckeditor5/34.0.0/classic/translations/zh.js"></script>
         <script type="text/javascript">
             class MyUploadAdapter {
                 constructor(loader) {
