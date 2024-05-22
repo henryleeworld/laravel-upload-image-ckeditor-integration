fbaa853f4198389cc8ac7a049e8661b39e68ee1d
Henry Lee <henryleeworld@gmail.com>
2024-05-23 00:40:10 +0800

    feat:更新最新資料

--- resources/views/ckeditor.blade.php
+++ resources/views/ckeditor.blade.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <title>{{ config('app.name') }}</title>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css" integrity="sha512-t4GWSVZO1eC8BM339Xd7Uphw5s17a86tIZIj8qRxhnKub6WoyhnrxeCIMeAqBPgdZGlCcG2PrZjMc+Wr78+5Xg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.3/css/bootstrap.min.css" integrity="sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <style>
             .ck-editor__editable_inline {
                min-height: 500px;
@@ -24,8 +24,8 @@
                 </div>
             </div>
         </div>
-        <script src="https://cdn.ckeditor.com/ckeditor5/38.1.1/classic/ckeditor.js"></script>
-        <script src="https://cdn.ckeditor.com/ckeditor5/38.1.1/classic/translations/zh.js"></script>
+        <script src="https://cdn.ckeditor.com/ckeditor5/41.1.0/classic/ckeditor.js"></script>
+        <script src="https://cdn.ckeditor.com/ckeditor5/41.1.0/classic/translations/zh.js"></script>
         <script type="text/javascript">
             class MyUploadAdapter {
                 constructor(loader) {
